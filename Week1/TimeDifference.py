f31972e38d09014e453fca736ae3b83b883cf1bf
Ruslan <37127343+RusS1103@users.noreply.github.com>
2019-04-02 13:25:10 +0300

    Create TimeDifference.py

---
+++ Week1/TimeDifference.py
@@ -0,0 +1,9 @@
+h1 = int(input())
+m1 = int(input())
+s1 = int(input())
+h2 = int(input())
+m2 = int(input())
+s2 = int(input())
+sum1 = h1 * 3600 + m1 * 60 + s1
+sum2 = h2 * 3600 + m2 * 60 + s2
+print(sum2 - sum1)
