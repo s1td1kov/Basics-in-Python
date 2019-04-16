41997b02538d54b09c76a28f9144a82632858684
Ruslan <37127343+RusS1103@users.noreply.github.com>
2019-04-16 16:31:54 +0300

    Create Флаги.py

---
+++ Week5/Флаги.py
@@ -0,0 +1,13 @@
+n = int(input())
+for i in range(n):
+    print('+___ ', end='')
+print()
+for i in range(n):
+    print('|', i + 1, ' ', '/', ' ', sep='', end='')
+print()
+for i in range(n):
+    print('|__\\ ', sep='', end='')
+print()
+for i in range(n):
+    print('|    ', end='')
+print()
