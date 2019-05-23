a57aa8b84805fcd2b42913bd3db987b6a2ad54ac
Ruslan <37127343+RusS1103@users.noreply.github.com>
2019-05-23 23:45:53 +0300

    Update Факториалы.py

--- Week8/Факториалы.py
+++ Week8/Факториалы.py
@@ -1,4 +1,13 @@
 from itertools import accumulate
 
 
-print(1, *accumulate(map(int, range(1, int(input()) + 1)), lambda x, y: x * y))
+print(
+    1, *accumulate(
+        map(
+            int,
+            range(
+                1, int(input()) + 1
+            )
+        ), lambda x, y: x * y
+    )
+)
